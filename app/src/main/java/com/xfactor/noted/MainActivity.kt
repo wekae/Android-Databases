8674802332090f389f8238766a0d31359a7e4c05
Victor Wekesa <wekae.dev@gmail.com>
2021-10-25 03:05:02 +0300

    Creating and setting up the room database

--- app/src/main/java/com/xfactor/noted/MainActivity.kt
+++ app/src/main/java/com/xfactor/noted/MainActivity.kt
@@ -1,17 +1,24 @@
 package com.xfactor.noted
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import androidx.room.Room
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.xfactor.noted.database.AppDatabase
+import kotlin.collections.List
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Initialize the DB - allow main thread queries for small apps
+        val db = Room.databaseBuilder(applicationContext, AppDatabase::class.java, "noted-database").allowMainThreadQueries().build()
 
         // Setting ActionBar logo
         supportActionBar?.setDisplayShowHomeEnabled(true)
@@ -27,6 +34,19 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_delete, R.id.navigation_listcontainer, R.id.navigation_newlist))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        // access the database
+        val listDao = db.listDao()
+        val lists : List<com.xfactor.noted.database.List> = listDao.getAll()
+
+        Log.e("lists", lists.toString())
+
+        // Update primary key value by incrementing value of the last item
+        listDao.insertAll(com.xfactor.noted.database.List(uid = lists.last().uid +1, title = "Test List Thre"))
+
+        listDao.delete(lists[0])
+
+        Log.e("lists", listDao.getAll().toString())
 
     }
 }
